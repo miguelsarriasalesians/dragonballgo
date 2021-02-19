4f85401108abb05c84b399d99f3c8b6739d3307a
Oriol Font <o.font@ratenow.es>
2021-02-19 19:57:03 +0100

    ListBallsScreen

---
+++ lib/screens/listBalls_screen.dart
@@ -0,0 +1,170 @@
+import 'package:dragonballgo/resources/dimens.dart';
+import 'package:dragonballgo/resources/palette_colors.dart';
+import 'package:dragonballgo/screens/login_screen.dart';
+import 'package:dragonballgo/screens/select_language_screen.dart';
+import 'package:dragonballgo/texts/texts.dart';
+import 'package:dragonballgo/views/icon_language.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+
+class ListBallsScreen extends StatelessWidget {
+  ListBallsScreen({this.title});
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: BallsBar()),
+      //BallContainer(Colors.grey),
+    );
+  }
+}
+
+class BallsBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 80,
+              width: 80,
+              child: Image(image: AssetImage('assets/images/mcball.png')),
+            ),
+            Text(
+              translate('listBalls_screen_title'),
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+            InkWell(
+              child: Icon(
+                Icons.settings_outlined,
+                size: 30,
+              ),
+              onTap: () {
+                //TODO LLEVA A LA CONFIGURACION DE TODA LA APP
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SelectLanguageScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+        Container(
+          color: Colors.white,
+          height: MediaQuery.of(context).size.height * 0.7,
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: Column(
+            children: [
+              RowBall(),
+              RowBall(),
+              RowBall(),
+              RowBall(),
+              RowBall(),
+              RowBall(),
+              RowBall(),
+            ],
+          ),
+        ),
+        Row (
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            InkWell(
+              child: Container(
+                height: 80,
+                width: 80,
+                child: Image(image: AssetImage('assets/images/world_map.png')),
+              ),
+              onTap: () {
+                //TODO NAVEGAR AL MAPA
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class BallContainer extends StatefulWidget {
+  final Color ContainerColorBall;
+  final String ballImg;
+
+  BallContainer(this.ContainerColorBall, this.ballImg);
+
+  @override
+  _BallContainerState createState() => _BallContainerState();
+}
+
+class _BallContainerState extends State<BallContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: (MediaQuery.of(context).size.height * 0.7) * 0.1425,
+      width: (MediaQuery.of(context).size.width * 0.9) * 0.3,
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: Colors.black),
+        color: widget.ContainerColorBall,
+      ),
+      child: Image(image: AssetImage(widget.ballImg)),
+    );
+  }
+}
+
+class TextContainer extends StatefulWidget {
+  final Color ContainerColor;
+
+  TextContainer(this.ContainerColor);
+
+  @override
+  _TextContainerState createState() => _TextContainerState();
+}
+
+class _TextContainerState extends State<TextContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: (MediaQuery.of(context).size.height * 0.7) * 0.1426,
+      width: (MediaQuery.of(context).size.width * 0.9) * 0.65,
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: Colors.black),
+        color: widget.ContainerColor,
+      ),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: new Text(
+          translate('information_ball'),
+          style: TextStyle(
+            color: PaletteColors.TEXT_BUTTON,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RowBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        BallContainer(Colors.grey, 'assets/images/unknown_ball.png'),
+        TextContainer(Colors.grey)
+      ],
+    );
+  }
+}
