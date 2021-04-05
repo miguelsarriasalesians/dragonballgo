013c9033684bfaca0d1b1f9dc2fbe603f3bedd4c
Miguel Peña <mpena@universaldoctor.com>
2021-04-05 11:29:57 +0200

    Llamando a api para pasar bolas al listBaslls desde login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
+import 'package:dragonballgo/objects/ball_model.dart';
 import 'package:dragonballgo/provider/api.dart';
 import 'package:dragonballgo/resources/palette_colors.dart';
 import 'package:dragonballgo/resources/routes.dart';
+import 'package:dragonballgo/utils/navigation_manager.dart';
 import 'package:dragonballgo/utils/router.dart';
 import 'package:dragonballgo/utils/session_manager.dart';
 import 'package:dragonballgo/widgets/custom_textfield.dart';
@@ -8,18 +10,31 @@ import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 
+import 'listBalls_screen.dart';
+
 class LoginScreen extends StatelessWidget {
   final emailController = new TextEditingController();
   final passwordController = new TextEditingController();
   final SessionManager manager = new SessionManager();
 
   void login(BuildContext ctx) {
-    FetchLogin(emailController.text, passwordController.text).then((val) {
+    FetchLogin(emailController.text, passwordController.text).then((val) async {
       if (val == 200) {
-        AppRouter.router.pop(ctx);
-        AppRouter.router.navigateTo(ctx, ScreenRoutes.BALLSLIST,
-            transition: TransitionType.fadeIn,
-            transitionDuration: Duration(milliseconds: 600));
+        //Get List of BallModel and pass it as parameter to ListBallsScreen
+
+        List<BallModel> balls = await getBalls();
+        if (balls.length != 0 || balls.length != null) {
+          NavigationManager(ctx).openScreenAsNew(ListBallsScreen(
+            listOfBalls: balls,
+          ));
+        } else {
+          print("EMPTY BODY");
+        }
+
+        // AppRouter.router.pop(ctx);
+        // AppRouter.router.navigateTo(ctx, ScreenRoutes.BALLSLIST,
+        //     transition: TransitionType.fadeIn,
+        //     transitionDuration: Duration(milliseconds: 600));
       } else {
         String text;
         switch (val) {
@@ -41,6 +56,27 @@ class LoginScreen extends StatelessWidget {
         ));
       }
     });
+  }
+
+  Future<List<BallModel>> getBalls() async {
+    String token = await manager.getToken();
+    Map<String, dynamic> balls = await FetchBalls(
+        latitude: 6.17790967, longitude: 16.17790967, token: token);
+
+    // List<dynamic> list = balls.values.toList();
+    List<BallModel> theBalls =
+        List<BallModel>.generate(balls["body"].length, (int index) {
+      Map currentBall = balls["body"][index];
+      return BallModel(
+          id: currentBall["num"],
+          latitude: currentBall["latitude"],
+          longitude: currentBall["longitude"],
+          pickedDate:
+              currentBall.containsKey("date") ? currentBall["date"] : null,
+          image:
+              currentBall.containsKey("image") ? currentBall["image"] : null);
+    });
+    return theBalls;
   }
 
   @override
@@ -92,7 +128,9 @@ class LoginScreen extends StatelessWidget {
                                 primary: PaletteColors.MAINCOLOR,
                                 shape: RoundedRectangleBorder(
                                     borderRadius: BorderRadius.circular(25))),
-                            onPressed: () => login(context),
+                            onPressed: () {
+                              login(context);
+                            },
                             child: Text(translate("login_screen_title"))),
                       ),
                       SizedBox(
