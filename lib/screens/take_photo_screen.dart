e17b7cd0f81861ebc0dab0dadebaa31d9f71362a
Miguel Peña <mpena@universaldoctor.com>
2021-04-08 16:00:55 +0200

    Creada base TakePhotoScreen

---
+++ lib/screens/take_photo_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TakePhotoScreen extends StatefulWidget {
+  @override
+  _TakePhotoScreenState createState() => _TakePhotoScreenState();
+}
+
+class _TakePhotoScreenState extends State<TakePhotoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
