cc9ce900ba73612687decbb69075a96363b9a169
Miguel Peña <mpena@universaldoctor.com>
2021-04-08 16:40:59 +0200

    Cogiendo camaras

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:camera/camera.dart';
 import 'package:dragonballgo/resources/routes.dart';
 import 'package:dragonballgo/utils/router.dart';
 import 'package:dragonballgo/utils/session_manager.dart';
@@ -14,6 +17,12 @@ var prefLanguageCode;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
 
   await SessionManager().getCountry().then((language) {
     if (language != null) {
